604c38e8dc30a028618f2071a81f4593d25fe8ec
juanapm123456-stack <juanapm123456@gmail.com>
2025-11-22 20:34:11 +0100

    que miedo

--- start.bat
+++ start.bat
@@ -3,14 +3,15 @@ echo ===============================================
 echo   Iniciando PixelShop con todas las variables
 echo ===============================================
 
-set RESEND_API_KEY=re_bwbKzPEJ_Joc6W4xS2jdBCyZN5S3MPN8g
+set EMAIL_USERNAME=juanapmparkersolution@gmail.com
+set EMAIL_PASSWORD=dbhd urmn vpoq vncs
 set CLOUDINARY_CLOUD_NAME=pixelshop
 set CLOUDINARY_API_KEY=742135482932556
 set CLOUDINARY_API_SECRET=k71G2RrGPrW09sSzmVRQ2PHt0K4
 set GOOGLE_CLIENT_ID=18168021278-s2kgou2l49vp2850ujup870qvpedv0em.apps.googleusercontent.com
 set GOOGLE_SECRET=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
-set PAYPAL_CLIENT_ID=AUJoWELN6zFo6AcAlcpHDjl2r-uXpjuIqLOK-5jCKJsYcIwRQwvx2WRzXS66p_7yFIyQUixHn0ZvNNEv
-set PAYPAL_CLIENT_SECRET=EHDn8X6Wmy0zPkwXpTzHlJ5SN0_jWmQiM1qFgTjvnCv7dJdOPxEPOBRXfaU-7NZpYoCKb4E06mPBvV-i
+set PAYPAL_CLIENT_ID=AfUAGvDCxWhgSUKFnRGu7pCh2si2_IS6MEe8xC1FEKD1288Yom4uZNbwB7UMba-7eafCDY7xSat8fmo2
+set PAYPAL_CLIENT_SECRET=EO5zFEnFMpEveATnGn5tS2-kzyH4JUjHctqzCdWUWsBAaQZ9_D7Jl4nxFyLgMUNTEceHbklMINhE0vip
 
 echo.
 echo ✅ Variables de entorno cargadas
